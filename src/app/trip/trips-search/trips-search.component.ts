d6c989704c1901b3005925d2af02a6a2159e4517
ThibautE <thibaut07600@gmail.com>
2018-01-21 22:44:21 +0100

    fix

--- src/app/trip/trips-search/trips-search.component.ts
+++ src/app/trip/trips-search/trips-search.component.ts
@@ -11,18 +11,12 @@ import 'rxjs/Rx';
 })
 export class TripsSearchComponent implements OnInit {
 
-<<<<<<< HEAD
 	private tripsFind : any[];
-=======
 	private tripsParams : any = {};
-	private tripsFind : any = {};
->>>>>>> 05c8a55435dddd35071df8d47a48cb54789e370f
-	private subscribe : any;
 
 	constructor(private tripService : TripService, private route: ActivatedRoute) { }
 
 	ngOnInit() {
-<<<<<<< HEAD
 		console.log('composant');
 		if(this.date = "undefined"){
 			this.date = "01-01-2018";
@@ -30,25 +24,20 @@ export class TripsSearchComponent implements OnInit {
 
 		let date1 : string;
 		console.log('date : ' + this.date)
-		//date1 = this.date.getDate() + '-' + this.date.getMonth() + '-' + this.date.getFullYear();
-		console.log("date :" + this.date);
 		this.route.params.subscribe((params : Params) => {
 			let paramsUrl = this.cityD + '/' + this.cityA + '/' + this.date;
-=======
-
-		this.tripsParams = {};
-		this.tripsFind = {};
+			this.tripsParams = {};
+			this.tripsFind = {};
+		}
 	}
 
 	onSubmit(){
 		let date : string;
-		//date = this.tripsParams.date.getDate() + '-' + this.tripsParams.date.getMonth() + '-' + this.tripsParams.date.getFullYear();
 		console.log("date :" + this.tripsParams.date);
 
 		this.route.params.subscribe((params : Params) => {
-			let paramsUrl = this.tripsParams.cityD + '/' + this.tripsParams.cityA + '/' + this.tripsParams.date;
->>>>>>> 05c8a55435dddd35071df8d47a48cb54789e370f
-			this.tripService.getTripByParams(paramsUrl).subscribe(res => this.tripsFind = res)
+		let paramsUrl = this.tripsParams.cityD + '/' + this.tripsParams.cityA + '/' + this.tripsParams.date;
+		this.tripService.getTripByParams(paramsUrl).subscribe(res => this.tripsFind = res)
 		});
 	}
 
