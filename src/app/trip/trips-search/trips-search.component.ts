8a6ae8a45e663fb39f03f1cf49d5fe83ff366ec5
Chaillot <anthony-34400@hotmail.fr>
2018-01-21 23:05:32 +0100

    fixfixfix

--- src/app/trip/trips-search/trips-search.component.ts
+++ src/app/trip/trips-search/trips-search.component.ts
@@ -19,25 +19,11 @@ export class TripsSearchComponent implements OnInit {
 	constructor(private tripService : TripService, private route: ActivatedRoute) { }
 
 	ngOnInit() {
-		console.log('composant');
-		if(this.date = "undefined"){
-			this.date = "01-01-2018";
-		}
-
-		let date1 : string;
-		console.log('date : ' + this.date)
-		//date1 = this.date.getDate() + '-' + this.date.getMonth() + '-' + this.date.getFullYear();
-		console.log("date :" + this.date);
-		this.route.params.subscribe((params : Params) => {
-			let paramsUrl = this.cityD + '/' + this.cityA + '/' + this.date;
-			this.tripsParams = {};
-			this.tripsFind = {};
+		this.tripsParams = {};
+		this.tripsFind = {};
 	}
 
 	onSubmit(){
-		let date : string;
-		//date = this.tripsParams.date.getDate() + '-' + this.tripsParams.date.getMonth() + '-' + this.tripsParams.date.getFullYear();
-		console.log("date :" + this.tripsParams.date);
 		this.route.params.subscribe((params : Params) => {
 			let paramsUrl = this.tripsParams.cityD + '/' + this.tripsParams.cityA + '/' + this.tripsParams.date;
 			this.tripService.getTripByParams(paramsUrl).subscribe(res => this.tripsFind = res)
