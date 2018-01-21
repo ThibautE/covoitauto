b2962934693fe84c7cd6fd820be5f60c47bad701
Chaillot <anthony-34400@hotmail.fr>
2018-01-21 23:00:54 +0100

    fix

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -15,23 +15,25 @@ export class RegisterComponent implements OnInit {
 
     newUser: any = {};
     message: string;
-    reponse: any;
+    reponse: boolean;
 
     constructor(private userService: UserService) { }
 
     ngOnInit() {
-
+        this.newUser = {};
+        this.reponse = false;
     }
 
     onSubmit() {
         
         if(this.newUser.firstname == undefined || this.newUser.lastname == undefined || this.newUser.password == undefined || this.newUser.age < 18 || this.newUser.email == undefined){
             this.message = "Un problème à eu lieu, veuillez remplir tous les champs obligatoires.";
+            this.reponse = false;
         }
         else {
             this.newUser.conducteur = Cookie.get('_id');
             this.userService.create(this.newUser).subscribe((res => {
-                this.reponse = res;
+                this.reponse = true;
                 this.message = "Votre inscription a bien été enregistré !";
             }));
         }		
